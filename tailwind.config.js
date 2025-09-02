7da73754f9adfe86127192f15b73ffee7a35a003
Gustavo Silva <127400986+Gugaa03@users.noreply.github.com>
2025-09-02 16:23:05 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        red: { 400: "#f87171" },
+        green: { 400: "#34d399" },
+        yellow: { 400: "#facc15" },
+        purple: { 400: "#a78bfa" },
+        blue: { 400: "#60a5fa" },
+      },
+    },
+  },
+  plugins: [],
+};
